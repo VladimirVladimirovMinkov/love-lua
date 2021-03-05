fb16d5150d827bc26666d6eabbac4f3468ff6677
VladimirVladimirovMinkov <51568939+VladimirVladimirovMinkov@users.noreply.github.com>
2021-03-05 15:12:24 +1300

    Update main.lua

--- LOVE/ping/main.lua
+++ LOVE/ping/main.lua
@@ -8,8 +8,8 @@ local score1 = 0
 local score2 = 0
 
 function love.update()
-	pad1_y = pad1_y + (bool[isDown"s"] - bool[isDown"w"]) * 2.5
-	pad2_y = pad2_y + (bool[isDown"down"] - bool[isDown"up"]) * 2.5
+	pad1_y = pad1_y + (bool[isDown"s"] - bool[isDown"w"]) * 2
+	pad2_y = pad2_y + (bool[isDown"down"] - bool[isDown"up"]) * 2
 	if pad1_y < -250 then pad1_y = -250 end
 	if pad1_y > 250 then pad1_y = 250 end
 	if pad2_y < -250 then pad2_y = -250 end
@@ -34,7 +34,6 @@ function love.update()
 		pad2_y = 0
 	end
 	if ball.x > 325 and ball.x < 350 and math.abs(pad2_y - ball.y) < 60 then
-		speed = speed + 0.5
 		ball.x = 325
 		ball.vx = -speed
 		ball.vy = ball.vy * 0.8 + math.random(-10, 10) / 10 * speed
