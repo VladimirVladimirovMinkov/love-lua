1af9d879088910f295f11237bb0b2dc33d977c98
VladimirVladimirovMinkov <51568939+VladimirVladimirovMinkov@users.noreply.github.com>
2021-03-22 15:54:46 +1300

    Update main.lua
    fixed speed and score and py

--- LOVE/ping/main.lua
+++ LOVE/ping/main.lua
@@ -1,6 +1,7 @@
 local isDown = love.keyboard.isDown
 local bool = { [true] = 1, [false] = 0 }
-local speed = 1
+local speedd = 1
+local speed = speedd
 local ball = { x = 0, y = 0, vx = -speed, vy = speed }
 local pad1_y = 0
 local pad2_y = 0
@@ -10,21 +11,24 @@ local score2 = 0
 function love.update()
 	pad1_y = pad1_y + (bool[isDown"s"] - bool[isDown"w"]) * 2
 	pad2_y = pad2_y + (bool[isDown"down"] - bool[isDown"up"]) * 2
+	
 	if pad1_y < -250 then pad1_y = -250 end
 	if pad1_y > 250 then pad1_y = 250 end
 	if pad2_y < -250 then pad2_y = -250 end
 	if pad2_y > 250 then pad2_y = 250 end
+	
 	ball.x = ball.x + ball.vx
 	ball.y = ball.y + ball.vy
-	if math.abs(ball.y) >= 285 then 
-		ball.vy = -ball.vy
-	end
+	
+	if math.abs(ball.y) >= 285 then ball.vy = -ball.vy end
+	
 	if ball.x < -325 and ball.x > -350 and math.abs(pad1_y - ball.y) < 60 then
-		speed = speed
+		speed = speed + 0.02
 		ball.x = -325
 		ball.vx = speed
 		ball.vy = ball.vy * 0.5 + math.random(-10, 10) / 20 * speed
 	end
+	
 	if ball.x < -415 then
 		ball.x = 0
 		ball.y = 0
@@ -32,12 +36,16 @@ function love.update()
 		score2 = score2 + 1
 		pad1_y = 0
 		pad2_y = 0
+		speed = speedd
 	end
+	
 	if ball.x > 325 and ball.x < 350 and math.abs(pad2_y - ball.y) < 60 then
+		speed = speed + 0.02
 		ball.x = 325
 		ball.vx = -speed
 		ball.vy = ball.vy * 0.8 + math.random(-10, 10) / 10 * speed
 	end
+	
 	if ball.x > 415 then
 		ball.x = 0
 		ball.y = 0
@@ -45,8 +53,10 @@ function love.update()
 		score1 = score1 + 1
 		pad1_y = 0
 		pad2_y = 0
+		speed = speedd
 	end
-	if score1 > 6 or score2 > 6 then
+	
+	if score1 + score2 > 9 then
 		print("player " .. (score1 > score2 and 1 or 2) .. " wins.")
 	end
 end
