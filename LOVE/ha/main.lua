d2099901132ed98d4e62ab04952b3459fcd2f80e
VladimirVladimirovMinkov <51568939+VladimirVladimirovMinkov@users.noreply.github.com>
2021-08-04 13:04:05 +1200

    Update main.lua
    changing from a line system w/ x, y to a prerendered pixel screen w/ x array with y value system to shorten the load time a bit

--- LOVE/ha/main.lua
+++ LOVE/ha/main.lua
@@ -3,31 +3,11 @@ local width = love.graphics.getWidth( )
 local height = love.graphics.getHeight( )
 local a = 0.5
 
-function lin(x1, y1, x2, y2, r, g, b, a)
-	local x = x1 - x2
-	local y = y1 - y2
-	local m = y/x
-	if m < 0 then
-		m= m*-1
-	end
-	
-	if x > y then
-		for i=1, x do
-			poi[#poi + 1] = {math.min(x1, x2)+(i), math.min(y1, y2)+(i*m), r, g, b, a}
-		end
-	else
-		m = 1/m
-		for i=1, y do
-			poi[#poi + 1] = {math.min(x1, x2)+(i*m), math.min(y1, y2)+(i), r, g, b, a}
-		end
-	end
-end
-
 function love.load()
 	for x=0, width do
 	poi[x] = {}
 		for y=0, height do
-			poi[x][y] = {x, y}
+			poi[x][y] = y
 		end
 	end
 end
@@ -38,7 +18,7 @@ end
 function love.draw()
 	for x=0, width do
 		for y=0, height do
-			love.graphics.points(poi[x][y])
+			love.graphics.points(x, poi[x][y])
 		end
 	end
 end
