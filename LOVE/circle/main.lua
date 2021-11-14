4586d311c044f54b13bc9500d86b3f379a96cb50
VladimirVladimirovMinkov <51568939+VladimirVladimirovMinkov@users.noreply.github.com>
2021-11-14 20:32:25 +1300

    more flexible with gen
    moved dot gen from load to update so could do things like move or expand

--- LOVE/circle/main.lua
+++ LOVE/circle/main.lua
@@ -3,15 +3,16 @@ local x, y = 200, 200
 local pixels = {}
 local pi = 3.1415
 
+rx, ry = rx*2, ry*2
+
 function love.load()
-	rx, ry = rx*2, ry*2
-	for i=0, ry*2 do
-		pixels[i] = {x+(math.cos(i*pi/rx)*rx/pi), y+(math.sin(i*pi/ry)*ry/pi)}
-	end
+	
 end
 
 function love.update()
-
+	for i=0, ry*2 do
+		pixels[i] = {x+(math.cos(i*pi/rx)*rx/pi), y+(math.sin(i*pi/ry)*ry/pi)}
+	end
 end
 
 function love.draw()
